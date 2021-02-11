560713463aad4794ad6245a4083d1ac6f9b05b90
randleem <erandle2211@gmail.com>
2021-02-11 19:00:50 +0000

    yay banner is there

--- bmd-app/src/Components/HeroBanner/HeroBanner.js
+++ bmd-app/src/Components/HeroBanner/HeroBanner.js
@@ -1,5 +1,5 @@
 //rfec - shortcut
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './HeroBanner.css';
 
@@ -8,12 +8,15 @@ import pic1 from '../../Images/pic1.jpg';
 import pic2 from '../../Images/pic2.jpg';
 
 function HeroBanner() {
+    const [announcements, setAnnouncements] = useState([`Don't forget to get your bike serviced regularly to prevent your bikes parts from wearing out`])
+
     return (
         <div className='hero-container'>
             {/* <img className='hero-image' src={pic2} alt='BMD at work'/> */}
             <div className='hero-content'>
-            <h1>Welcome to Bike Man Dan</h1>
-            <h3>Anouncement 1</h3>
+            <h1 className='hero-welcome'>Welcome to Bike Man Dan</h1>
+            <br></br>
+            <h3 className='hero-announcement'>{announcements}</h3>
             </div>
             
         </div>
