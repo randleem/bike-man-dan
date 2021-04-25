dd437aa0b41d5cf2aa49b4056dafb771ab54d3e1
randleem <erandle2211@gmail.com>
2021-04-25 07:58:30 +0100

    airtable attempt

--- bmd-app/src/AirTable.js
+++ bmd-app/src/AirTable.js
@@ -1,21 +1,82 @@
+require('dotenv').config();
+
 var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyLnp2ozEQJQT4fY'}).base('app7bXGxuMvf82oJb');
+const airTableAPI = process.env.AIRTABLE_API_KEY;
+const airTableBaseId = process.env.AIRTABLE_BASE_ID;
 
-base('Price List').select({
-    // Selecting the first 3 records in Grid view:
-    view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
+// const base = require('airtable').base('appXInkHESEFZc80m');
 
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('Job'));
+// var Airtable = require('airtable');
+// Airtable.configure({
+//     endpointUrl: 'https://api.airtable.com',
+//     apiKey: `keyLnp2ozEQJQT4fY`
+// });
+// var base = Airtable.base('appXInkHESEFZc80m');
+
+
+var base = new Airtable({apiKey: "keyLnp2ozEQJQT4fY"}).base('appXInkHESEFZc80m');
+
+// base('Customer').select({
+//     // Selecting the first 3 records in Grid view:
+//     maxRecords: 3,
+//     view: "Grid view"
+// }).eachPage(function page(records, fetchNextPage) {
+//     // This function (`page`) will get called for each page of records.
+
+//     records.forEach(function(record) {
+//         console.log('Retrieved', record.get('email'));
+//     });
+
+//     // To fetch the next page of records, call `fetchNextPage`.
+//     // If there are more records, `page` will get called again.
+//     // If there are no more records, `done` will get called.
+//     fetchNextPage();
+
+// }, function done(err) {
+//     if (err) { console.error(err); return; }
+// });
+
+
+base('Customer').create([
+    {
+      "fields": {
+        "Last Name": "Leach",
+        "First Name": "Hannah",
+        "email": "hannahleach@gmail.com",
+        "Mobile Number": "07723541312",
+      }
+    }
+  ], function(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    records.forEach(function (record) {
+      console.log(record.get("Name"));
     });
+  });
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
+  base('Customer').update([
+    {
+      "id": "recq8lnaWhQwY67r9",
+      "fields": {
+        "Last Name": "Randle",
+        "First Name": "Emma"
+      }
+    },
+    {
+      "id": "rec3aLKb03vthaU3H",
+      "fields": {
+        "Last Name": "Hayward",
+        "First Name": "Daniel"
+      }
+    }
+  ], function(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    records.forEach(function(record) {
+      console.log(record.get('Last Name'));
+    });
+  });
